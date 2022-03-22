ee59663edad5efa07bc28c98d99374ebeed2f64f
FacuTaborra <taborrafacundo@gmail.com>
2022-03-22 17:35:08 -0300

    agrego header

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/Header.jsx'
-//import Footer from './components/footer/Footer.js'
+import Footer from './components/footer/Footer.js'
 //import products from './components/products/Products.js'
 
 function App() {
   return (
-      <Switch>
-        <Route path="/" component = {Header}/>
-      </Switch>
+      <div>
+        <Header/>
+        <Switch>
+
+        </Switch>
+        {<Footer/>}
+      </div>
   );
 }
 
