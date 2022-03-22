6da6ea6a5b017fde17f93082828e572b4ab3c422
FacuTaborra <taborrafacundo@gmail.com>
2022-03-22 17:39:53 -0300

    arreglo rutas

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import { Link, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/Header.jsx'
 import Footer from './components/footer/Footer.js'
-//import products from './components/products/Products.js'
+import Products from "./components/products/Products.jsx";
+import Users from "./components/users/Users.jsx";
+import Categories from "./components/categories/Categories.jsx";
 
 function App() {
   return (
       <div>
         <Header/>
         <Switch>
-
+          <Route path = "/products" component={Products} />
+          <Route path = "/categories" component={Categories}/>
+          <Route path = "/users" component={Users} />
         </Switch>
-        {<Footer/>}
+        <Footer/>
       </div>
   );
 }
