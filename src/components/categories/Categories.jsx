4c3db3b9ead66270a3e73583fddd57dcb4cdefcb
FacuTaborra <taborrafacundo@gmail.com>
2022-03-21 21:29:33 -0300

    agrego header con hipervinculos

---
+++ src/components/categories/Categories.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link, Route, Router, Switch } from 'react-router-dom';
+import './categories.css'
+
+function Categories(){
+    return (
+        <div className="categories">
+
+        </div>
+    );
+}
+
+export default Categories;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,37 @@
 import React from "react";
 import { Link, Route, Switch } from 'react-router-dom';
+import logo from './logo4.png'
 import './header.css'
+import Products from "../products/Products.jsx";
+import Users from "../users/Users.jsx";
+import Categories from "../categories/Categories.jsx";
 
 function Header(){
     return (
-        <div className="header">
-            <span>hola</span>
-        </div>
+        <header className="header">
+            <div className="header-ontop">
+                <div className = "content">
+                    <h3>Scaloneta DashBoard</h3>
+                </div>
+            </div>
+
+            <div class="header-top">
+                <div class="header-left">
+                    <img src={logo} alt="" class="logo"/>
+                </div>
+                <div className="links">
+                    <Link to = "/products">Productos</Link>
+                    <Link to = "/categories">Categorias</Link>
+                    <Link to = "/users">Usuarios</Link>
+                    <Route path = "/products" component={Products} />
+                    <Route path = "/categories" component={Categories}/>
+                    <Route path = "/users" component={Users} />
+                </div>
+                <div class="header-center-simple">
+                    <h4>SCALONETA SHOP</h4>
+                </div>
+            </div>
+        </header>
     );
 }
 
