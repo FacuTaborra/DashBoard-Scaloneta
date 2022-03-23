b6c328de0c4318b27ca7a557ea29ecd6c53186e9
EstebanKa <estebankarlenaguirre@gmail.com>
2022-03-23 13:37:09 -0300

    refactor

--- src/App.js
+++ src/App.js
@@ -3,15 +3,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/header/Header.jsx'
 import Footer from './components/footer/Footer.jsx'
-import ContentProducts from './components/contentTop/ContentProducts.jsx';
-import ContentTop from './components/contentTop/contentTop';
 //import products from './components/products/Products.js'
 
 function App() {
   return (
       <div>
         <Header/>
-        <ContentTop/>
+        
         <Footer/>
       </div>
   );
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import './footer.css';
+import logo from '../header/logo4.png'
 
 function Footer(){
     return(
         
     <footer className="footer">
-            
+        <div className="box">
+            <img src={logo} alt="" class="logo2"/>
             <span>Copyright &copy; ScalonetaShop</span>
-            
+            <img src={logo} alt="" class="logo2"/>
+        </div>
         
     </footer>
     )
